3eff7272b820515a04e32eba6c63741c1a05da1d
Jonas Wagner <jonas@29a.ch>
2021-09-01 11:03:59 +0200

    typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="smartcrop" />
+
+import { CropOptions, CropResult } from "smartcrop";
+
+/**
+ * Find the best crop for *image* using *options*.
+ * @param image - A string (path to file) or buffer of image source.
+ * @param cropOptions - Crop options based to `smartcrop.js`.
+ * @return Promise that resolves with the final crop result that can then be
+ * used by `sharp` to extract your final image.
+ */
+export function crop(image: Buffer | string, cropOptions: CropOptions): Promise<CropResult>;
